fcfe2add49667302eba41e6d2f9bcfdd81d7de7f
Paragon <94728317+RbxParagon@users.noreply.github.com>
2026-08-27 23:26:12 -0500

    Create offsets.hpp

---
+++ loader/offsets.hpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+
+// version-f5a60436d48947d3
+
+namespace offsets {
+
+    inline static constexpr std::uintptr_t name = 0x70;
+    inline static constexpr std::uintptr_t children = 0x78;
+    inline static constexpr std::uintptr_t parent = 0x68;
+    inline static constexpr std::uintptr_t self = 0x8;
+    inline static constexpr std::uintptr_t classDescriptor = 0xF8;
+    // removed other stuff find it yourself (use IDA or cheat engine)
+
+}
